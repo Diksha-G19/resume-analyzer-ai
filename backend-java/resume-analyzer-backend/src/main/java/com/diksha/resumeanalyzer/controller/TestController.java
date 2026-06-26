13fa1c5e8512240dc2aade3d584a672a1b2f9559
Diksha-G19 <diksha941010@gmail.com>
2026-06-26 16:00:59 +0530

    Setup Spring Boot backend and PostgreSQL

---
+++ backend-java/resume-analyzer-backend/src/main/java/com/diksha/resumeanalyzer/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.diksha.resumeanalyzer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public Map<String, String> test() {
+
+        return Map.of(
+                "message",
+                "Backend is working!"
+        );
+    }
+}
